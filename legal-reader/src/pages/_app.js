c1b0a37c6fab48505a8f0bf57155877c49ac275c
utkarsh-vats <utkarsh.vats.2k@gmail.com>
2024-04-14 08:48:23 +0530

    Day 1 - Basic Setup - 02

--- legal-reader/src/pages/_app.js
+++ legal-reader/src/pages/_app.js
@@ -1,5 +1,6 @@
 // import "@/styles/globals.css";
 import "@/styles/global.css";
+// import "@/styles/FontAwesome.css";
 
 export default function App({ Component, pageProps }) {
 	return <Component {...pageProps} />;
--- legal-reader/src/pages/index.js
+++ legal-reader/src/pages/index.js
@@ -19,9 +19,25 @@ export default function Home() {
 					content="width=device-width, initial-scale=1"
 				/>
 				<link rel="icon" href="/Icons/folders.ico" />
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+					integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+					crossorigin="anonymous"
+					referrerpolicy="no-referrer"
+				/>
 			</Head>
-			<main className={`${styles.main} ${inter.className}`}>
-				<div className={styles.container}>Hello!</div>
+			<main className={`${styles.bodyContainer} ${inter.className}`}>
+				<div className={styles.container}>
+					<div className={styles.inputContainer}>
+						<input
+							className={styles.queryInput}
+							type="text"
+							id="queryField"
+						/>
+						<button className="sttInput"></button>
+					</div>
+				</div>
 			</main>
 		</>
 	);
